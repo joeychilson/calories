de9938e75efee8dbaaf4f1baad2822ebe55573cb
Joey Chilson <joeychilson@outlook.com>
2025-12-03 03:08:50 -0600

    Add padding

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -44,7 +44,7 @@
 <div class="flex h-full flex-col bg-background">
 	<div class="mx-auto flex h-full w-full max-w-md flex-col">
 		<DayNavigation date={selectedDate} onDateChange={() => updateUrlDate(selectedDate)} />
-		<div class="flex min-h-0 flex-1 flex-col">
+		<div class="flex min-h-0 flex-1 flex-col px-4">
 			<div class="flex min-h-0 flex-1 flex-col gap-3">
 				<CalorieSummary date={selectedDateStr} />
 				<WeightTracker date={selectedDateStr} />
